abd022347cce5717faa3463f017790506f88b47f
kwr1337 <rostft@gmail.com>
2025-04-26 15:57:54 +0300

    1

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ BOT_TOKEN = '8170950920:AAFZe0N3gKxhXIKsaGL3KW3gaYeTItnGfzY'
 DATABASE_PATH = 'library.db'
 
 # URL веб-панели администратора
-ADMIN_PANEL_URL = "http://localhost:8000"  # Измените на ваш URL, если он отличается 
+ADMIN_PANEL_URL = "http://45.10.41.224:3080/"  # Измените на ваш URL, если он отличается 
